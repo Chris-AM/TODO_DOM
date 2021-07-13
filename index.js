9dd3e3b60307dac42d1c22d97da2bb886829a036
Chris-AM <c.aranguizm@outlook.com>
2021-07-13 13:29:17 -0400

    now can delete and persitant data in TODO list

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const todoArr = [];
+const todoArr = JSON.parse(localStorage.getItem('todoArr')) || [];
+const render = () => {
+    const todoList = document.getElementById('todo-list');
+    const todoTemplate = todoArr.map(newTODO => '<li>' + newTODO + '</li>');
+    todoList.innerHTML = todoTemplate.join('');
+    const getToDo = document.querySelectorAll('#todo-list li');
+    getToDo.forEach((element, index) => {
+        element.addEventListener('click', () => {
+            element.parentNode.removeChild(element);
+            todoArr.splice(index, 1);
+            render();
+            console.log(todoArr, index);
+        })
+    });
+}
 window.onload = () => {
+    render();
     const form = document.getElementById('todo-form');
     form.onsubmit = (e) => {
         e.preventDefault();
@@ -7,11 +22,8 @@ window.onload = () => {
         const todoText = todo.value;
         todo.value = '';
         todoArr.push(todoText);
-        console.log(todoArr);
-        const todoList = document.getElementById('todo-list');
-        todoList.innerHTML = '';
-        for(let i = 0; i < todoArr.length; i++){
-            todoList.innerHTML += '<li>' + todoArr[i] + '</li>';
-        }
+        const todoStrings = JSON.stringify(todoArr);
+        localStorage.setItem('todoArr', todoStrings);
+        render();
     }
 }
